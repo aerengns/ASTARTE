431135f6495c9bc2c095ce90186751123edc6b33
Ahmet Eren Genis <e2448389@ceng.metu.edu.tr>
2022-11-15 12:02:24 +0300

    Added static sign in layout as an extra file

---
+++ flutter_test/flutter_init/my_app/lib/sign_in.dart
@@ -0,0 +1,279 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Column(children: [
+      Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    'ASTARTE',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red[700],
+                        fontSize: 35),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      Row(
+        children: [
+          Expanded(
+              child: Container(
+                  padding: const EdgeInsets.only(top: 30, left: 80),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Sign In',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                            fontSize: 25),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Text(
+                          'Hi there! Nice to see you again.',
+                          style:
+                          TextStyle(color: Colors.grey[500], fontSize: 15),
+                        ),
+                      )
+                    ],
+                  )))
+        ],
+      )
+    ]);
+
+    Widget credentialsSection = Container(
+      padding: const EdgeInsets.only(top: 20, left: 80),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              'Email',
+              style: TextStyle(
+                  color: Colors.red, fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(
+                'example@email.com',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Container(
+                height: 2.0,
+                width: 300.0,
+                color: Colors.grey,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                'Password',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                      '********************',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Icon(
+                    Icons.remove_red_eye_sharp,
+                    color: Colors.black26,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Container(
+                height: 2.0,
+                width: 300.0,
+                color: Colors.grey,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Container(
+                alignment: Alignment.center,
+                height: 50,
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 300,
+              padding: const EdgeInsets.only(top: 30),
+              child: Text(
+                'or use one of your social profiles',
+                style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 300,
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 140,
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          FontAwesomeIcons.twitter,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Twitter',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 140,
+                    decoration: const BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          // Use the FaIcon Widget + FontAwesomeIcons class for the IconData
+                            icon: const FaIcon(
+                              FontAwesomeIcons.facebook,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              print("Pressed");
+                            }),
+                        const Text(
+                          'Facebook',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 60),
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17),
+                  ),
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+    );
+
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/images/astarte.jpg',
+              width: 100,
+              height: 100,
+              fit: BoxFit.contain,
+            ),
+            titleSection,
+            credentialsSection,
+          ],
+        ),
+      ),
+    );
+  }
+}
