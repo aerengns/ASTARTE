1fd89c590c9ab23671569d4344b3a338cfacb02a
Aylin Topcu <topcu.aylinn@gmail.com>
2022-11-27 15:32:30 +0300

    initialize firebase project for previously built sign in screen

--- flutter_test/flutter_init/my_app/lib/main.dart
+++ flutter_test/flutter_init/my_app/lib/main.dart
@@ -1,10 +1,97 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 import 'package:flutter/material.dart';
-// Uncomment lines 3 and 6 to view the visual layout at runtime.
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  // debugPaintSizeEnabled = true;
   runApp(const MyApp());
+}
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  bool _isHidden = true;
+
+  void _handleTap() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const SizedBox(
+          width: 300,
+          child: TextField(
+            obscureText: false,
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              labelStyle: TextStyle(
+                  color: Colors.red, fontSize: 15, fontWeight: FontWeight.bold),
+              labelText: 'Email',
+              hintText: 'example@email.com',
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 300,
+          child: SizedBox(
+            width: 300,
+            child: TextField(
+              obscureText: _isHidden,
+              decoration: InputDecoration(
+                border: const UnderlineInputBorder(),
+                labelStyle: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+                labelText: 'Password',
+                suffix: IconButton(
+                  onPressed: _handleTap,
+                  icon: const Icon(
+                    Icons.remove_red_eye_sharp,
+                    color: Colors.black26,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: TextButton(
+            onPressed: () {},
+            child: Container(
+              alignment: Alignment.center,
+              height: 50,
+              width: 300,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Sign In',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,155 +99,196 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
+    Widget titleSection = Column(children: [
+      Row(
         children: [
           Expanded(
             /*1*/
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 /*2*/
                 Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
+                  padding: const EdgeInsets.only(top: 8, bottom: 30),
+                  child: Text(
+                    'ASTARTE',
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red[700],
+                        fontSize: 35),
                   ),
                 ),
               ],
             ),
           ),
-          const FavoriteWidget(),
         ],
       ),
-    );
+      Row(
+        children: [
+          Expanded(
+              child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 60),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Sign In',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                            fontSize: 25),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Text(
+                          'Hi there! Nice to see you again.',
+                          style:
+                              TextStyle(color: Colors.grey[500], fontSize: 15),
+                        ),
+                      )
+                    ],
+                  )))
+        ],
+      )
+    ]);
 
-    Color color = Theme.of(context).primaryColor;
+    Widget credentialsSection = Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 60),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SignInForm(),
 
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
+            Container(
+              alignment: Alignment.center,
+              width: 300,
+              padding: const EdgeInsets.only(top: 30),
+              child: Text(
+                'or use one of your social profiles',
+                style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 300,
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 140,
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          Icon(
+                            FontAwesomeIcons.twitter,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Twitter',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 140,
+                    decoration: const BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          FaIcon(
+                            FontAwesomeIcons.facebook,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Facebook',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 60),
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
     );
 
     return MaterialApp(
-      title: 'Flutter layout demo',
+      title: 'Flutter Layout Demo',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
         body: ListView(
           children: [
             Image.asset(
-              'assets/images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
+              'assets/images/astarte.jpg',
+              width: 100,
+              height: 100,
+              fit: BoxFit.contain,
             ),
             titleSection,
-            buttonSection,
-            textSection,
+            credentialsSection,
           ],
         ),
       ),
-    );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class FavoriteWidget extends StatefulWidget {
-  const FavoriteWidget({super.key});
-
-  @override
-  State<FavoriteWidget> createState() => _FavoriteWidgetState();
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            padding: const EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isFavorited
-                ? const Icon(Icons.star)
-                : const Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
-          ),
-        ),
-        SizedBox(
-          width: 18,
-          child: SizedBox(
-            child: Text('$_favoriteCount'),
-          ),
-        ),
-      ],
     );
   }
 }
