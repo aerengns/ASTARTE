53b27a85884f3b10da77e0503cdd182f6808bd5c
Aylin Topcu <topcu.aylinn@gmail.com>
2022-12-10 22:34:08 +0300

    Implement sign in using email and password

--- flutter_test/flutter_init/my_app/lib/main.dart
+++ flutter_test/flutter_init/my_app/lib/main.dart
@@ -4,6 +4,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_app/validator.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -23,6 +25,11 @@ class SignInForm extends StatefulWidget {
 
 class _SignInFormState extends State<SignInForm> {
   bool _isHidden = true;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success = false;
+  String _userEmail = '';
 
   void _handleTap() {
     setState(() {
@@ -32,71 +39,120 @@ class _SignInFormState extends State<SignInForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        const SizedBox(
-          width: 300,
-          child: TextField(
-            obscureText: false,
-            decoration: InputDecoration(
-              border: UnderlineInputBorder(),
-              labelStyle: TextStyle(
-                  color: Colors.red, fontSize: 15, fontWeight: FontWeight.bold),
-              labelText: 'Email',
-              hintText: 'example@email.com',
+    return Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+                width: 300,
+                child: TextFormField(
+                  controller: _emailController,
+                  validator: (value) => Validator.validateEmail(email: value ?? ''),
+                  obscureText: false,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelStyle: TextStyle(
+                        color: Colors.red, fontSize: 15, fontWeight: FontWeight.bold),
+                    labelText: 'Email',
+                    hintText: 'example@email.com',
+                  ),
+                )
             ),
-          ),
-        ),
-        SizedBox(
-          width: 300,
-          child: SizedBox(
-            width: 300,
-            child: TextField(
-              obscureText: _isHidden,
-              decoration: InputDecoration(
-                border: const UnderlineInputBorder(),
-                labelStyle: const TextStyle(
-                    color: Colors.red,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold),
-                labelText: 'Password',
-                suffix: IconButton(
-                  onPressed: _handleTap,
-                  icon: const Icon(
-                    Icons.remove_red_eye_sharp,
-                    color: Colors.black26,
+            SizedBox(
+              width: 300,
+              child: SizedBox(
+                width: 300,
+                child: TextFormField(
+                  controller: _passwordController,
+                  validator: (value) => Validator.validatePassword(password: value ?? ''),
+                  obscureText: _isHidden,
+                  decoration: InputDecoration(
+                    border: const UnderlineInputBorder(),
+                    labelStyle: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                    labelText: 'Password',
+                    suffix: IconButton(
+                      onPressed: _handleTap,
+                      icon: const Icon(
+                        Icons.remove_red_eye_sharp,
+                        color: Colors.black26,
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 30),
-          child: TextButton(
-            onPressed: () {},
-            child: Container(
-              alignment: Alignment.center,
-              height: 50,
-              width: 300,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
+            Container(
+              padding: const EdgeInsets.only(top: 30),
+              child: TextButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    _signInWithEmailAndPassword();
+                  }
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
                 ),
               ),
-              child: const Text(
-                'Sign In',
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
             ),
-          ),
-        ),
-      ],
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                _success == null
+                    ? ''
+                    : (_success
+                    ? 'Successfully signed in $_userEmail'
+                    : 'Sign in failed'),
+                style: TextStyle(color: Colors.black),
+              ),
+            )
+          ],
+        )
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _signInWithEmailAndPassword() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = (await auth.signInWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    )).user;
+
+    if (user != null) {
+      setState(() {
+        _success = true;
+        _userEmail = user.email!;
+      });
+    } else {
+      setState(() {
+        _success = false;
+      });
+    }
   }
 }
 
