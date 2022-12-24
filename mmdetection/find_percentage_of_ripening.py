054bc8b6ee51c9553041eb97bdc3f10912bb0b7d
Mustafa Orhun Kose <orhun.kose@radius.ai>
2022-12-24 15:14:41 +0300

    Minor changes

--- mmdetection/find_percentage_of_ripening.py
+++ mmdetection/find_percentage_of_ripening.py
@@ -1,7 +1,4 @@
 from mmdet.apis import init_detector, inference_detector, show_result_pyplot
-import mmcv
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 config_file = 'configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py'
 checkpoint_file = 'checkpoints/laboro_tomato_48ep.pth'
@@ -18,7 +15,6 @@ result = inference_detector(model, img)
 # plt.figure(figsize = (10,15))
 # plt.imshow(image, aspect='auto')
 
-show_result_pyplot(model, img, result, score_thr=0.7)
 
 
 ripening_dict = {0: 1,
@@ -43,7 +39,11 @@ for i, row in enumerate(result[0]):
     total_ripness += num_of_tomato*ripening_dict[i]
     number_of_tomatoes_dict[i] += num_of_tomato
 
-print(f'Percentage of ripeness: {100*total_ripness/total_detection:.2f}% \n \
+if(total_detection == 0): print('no detection')
+else:
+    print(f'Percentage of ripeness: {100*total_ripness/total_detection:.2f}% \n \
 total number of fully ripened tomatoes: {number_of_tomatoes_dict[0] + number_of_tomatoes_dict[3]}\n \
 total number of half ripened tomatoes: {number_of_tomatoes_dict[1] + number_of_tomatoes_dict[4]}\n \
 total number of green tomatoes: {number_of_tomatoes_dict[2] + number_of_tomatoes_dict[5]}')
+
+show_result_pyplot(model, img, result, score_thr=0.7)
