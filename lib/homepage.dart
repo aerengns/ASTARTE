d96bea55421f32b7a3e335150af60b36c9728f0a
Aylin Topcu <topcu.aylinn@gmail.com>
2022-12-27 22:08:13 +0300

    add methods to home page to track notification count

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:astarte/sidebar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:astarte/notificationBadge.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +13,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late int _totalNotifications;
+  PushNotification? _notificationInfo;
+  late final FirebaseMessaging _messaging;
+
+  @override
+  void initState() {
+    _totalNotifications = 0;
+    super.initState();
+  }
+
+  void registerNotification() async {
+    // 1. Initialize the Firebase app
+    await Firebase.initializeApp();
+
+    // 2. Instantiate Firebase Messaging
+    _messaging = FirebaseMessaging.instance;
+
+    // 3. On iOS, this helps to take the user permissions
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: Duration(seconds: 2),
+          );
+        }
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body,
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+      });
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,8 +104,18 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    IconButton(onPressed: (){}, icon:  const Icon(Icons.water_drop_rounded, size: 100,color:Colors.blueAccent)),
+                    IconButton(onPressed: (){}, icon:  const Icon(Icons.water_drop_rounded, size: 100, color: Colors.blueAccent)),
                     IconButton(onPressed: (){}, icon: const Icon(Icons.pest_control_rounded, size: 100, color: Colors.brown,)),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 100,),
+              Padding(
+                padding: const EdgeInsets.only(right: 100.0, left: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    NotificationBadge(totalNotifications: _totalNotifications),
                   ],
                 ),
               ),
@@ -57,10 +126,13 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-
-
-
-
-
+class PushNotification {
+  PushNotification({
+    this.title,
+    this.body,
+  });
+  String? title;
+  String? body;
+}
 
 
