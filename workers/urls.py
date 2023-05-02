8f9b0f897b763f7689f8a663505a5b454adae2db
Ahmet Eren Genis <e2448389@ceng.metu.edu.tr>
2023-05-02 17:16:54 +0300

    Added job assigning to Workers Page

--- workers/urls.py
+++ workers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('workers_data', views.WorkerDataAPI.as_view(), name='workers-data'),
+    path('jobs_data', views.JobDataAPI.as_view(), name='jobs-data'),
 ]
