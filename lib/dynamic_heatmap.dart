b521e035462092e8c4816afbef14c7333d39cdc8
Mustafa Orhun Köse <e2380707@ceng.metu.edu.tr>
2023-05-09 15:29:29 +0300

    Add switch button between heatmap modes

--- lib/dynamic_heatmap.dart
+++ lib/dynamic_heatmap.dart
@@ -6,6 +6,14 @@ import 'package:astarte/utils/dynamic_heatmap_utils.dart';
 
 import 'package:astarte/sidebar.dart';
 
+bool isExist(List<List<int>> sensorLocations, List<int> currLocation) {
+  for (int i = 0; i < sensorLocations.length; i++) {
+    if (sensorLocations[i][0] == currLocation[0] &&
+        sensorLocations[i][1] == currLocation[1]) return true;
+  }
+  return false;
+}
+
 class HeatmapPage extends StatefulWidget {
   const HeatmapPage({Key? key}) : super(key: key);
 
@@ -15,10 +23,14 @@ class HeatmapPage extends StatefulWidget {
 
 class _DynamicHeatmapState extends State<HeatmapPage> {
   HeatmapItem? selectedItem;
+  bool isTrue = true;
+
+  late Widget dynamic_heatmap;
+  late Image heatmap_image;
 
   @override
   void initState() {
-    _initExampleData();
+    dynamic_heatmap = _initExampleData();
     super.initState();
   }
 
@@ -27,51 +39,56 @@ class _DynamicHeatmapState extends State<HeatmapPage> {
     return FutureBuilder<DynamicHeatmap>(
       future: getHeatmapData(),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          DynamicHeatmap? hmap = snapshot.data;
-          List<String> rows = hmap!.rows;
-          List<String> columns = hmap.columns;
+        List<String> rows;
+        List<String> columns;
+        List<double> values;
+        List<List<int>> sensor_locations;
 
-          const String unit = 'moisture';
-          final items = [
-            for (int row = 0; row < rows.length; row++)
-              for (int col = 0; col < columns.length; col++)
-                if (!(row == 3 &&
-                    col <
-                        2)) // Do not add the very first item (incomplete data edge case)
-                  HeatmapItem(
-                      value: hmap.items[row],
-                      style: row == 3 && col == 2 ||
-                              row == 1 && col == 3 ||
-                              row == 2 && col == 6 ||
-                              row == 1 && col == 9
-                          ? HeatmapItemStyle.hatched
-                          : HeatmapItemStyle.filled,
-                      unit: unit,
-                      xAxisLabel: columns[col],
-                      yAxisLabel: rows[row]),
-          ];
-          heatmapDataPower = HeatmapData(
-            rows: rows,
-            columns: columns,
-            radius: 6.0,
-            items: items,
-          );
+        const String unit = 'moisture';
+        List<int> current_location;
+        final List<HeatmapItem> items = [];
+
+        DynamicHeatmap? hmap = snapshot.data;
+
+        if (snapshot.hasData) {
+          rows = hmap!.rows;
+          columns = hmap.columns;
+          values = hmap.items;
+          sensor_locations = hmap.sensor_locations;
+          heatmap_image = hmap.heatmap_image;
         } else {
-          heatmapDataPower = const HeatmapData(
-            rows: ['deneme'],
-            columns: ['deneme'],
-            radius: 6.0,
-            items: [
-              HeatmapItem(
-                  value: -1,
-                  style: HeatmapItemStyle.filled,
-                  unit: 'deneme',
-                  xAxisLabel: 'deneme',
-                  yAxisLabel: 'deneme')
-            ],
+          heatmap_image = Image.asset(
+            'assets/icons/launcher_icon.png',
+            fit: BoxFit.cover,
           );
+          return Placeholder();
         }
+
+        for (int row = 0; row < rows.length; row++) {
+          for (int col = 0; col < columns.length; col++) {
+            current_location = [col, (rows.length - 1 - row)];
+            if (!(values[(rows.length - 1 - row) * columns.length + col] ==
+                0)) {
+              items.add(HeatmapItem(
+                  value: values[(rows.length - 1 - row) * columns.length + col],
+                  style: isExist(sensor_locations, current_location)
+                      ? HeatmapItemStyle.hatched
+                      : HeatmapItemStyle.filled,
+                  unit: unit,
+                  xAxisLabel: columns[col],
+                  yAxisLabel: rows[row]));
+            }
+          }
+        }
+
+        heatmapDataPower = HeatmapData(
+          rows: rows,
+          columns: columns,
+          radius: 1,
+          items: items,
+          colorPalette: colorPaletteBlue,
+        );
+
         return Heatmap(
             onItemSelectedListener: (HeatmapItem? selectedItem) {
               debugPrint(
@@ -81,16 +98,21 @@ class _DynamicHeatmapState extends State<HeatmapPage> {
               });
             },
             heatmapData: heatmapDataPower);
-        ;
       },
     );
+  }
+
+  void _switchImage() {
+    setState(() {
+      isTrue = !isTrue;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     final title = selectedItem != null
-        ? '${selectedItem!.value.toStringAsFixed(2)} ${selectedItem!.unit}'
-        : '--- deneme';
+        ? '${(selectedItem!.value * (100 / 9)).toStringAsFixed(2)} ${selectedItem!.unit}'
+        : '--- moisture';
     final subtitle = selectedItem != null
         ? '${selectedItem!.xAxisLabel} ${selectedItem!.yAxisLabel}'
         : '---';
@@ -103,7 +125,12 @@ class _DynamicHeatmapState extends State<HeatmapPage> {
             Text(title, textScaleFactor: 1.4),
             Text(subtitle),
             const SizedBox(height: 8),
-            _initExampleData()
+            isTrue ? dynamic_heatmap : heatmap_image,
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _switchImage,
+              child: Text("Switch"),
+            ),
           ],
         ),
       ),
--- lib/utils/dynamic_heatmap_utils.dart
+++ lib/utils/dynamic_heatmap_utils.dart
@@ -11,10 +11,17 @@ import 'calendar_utils.dart';
 class DynamicHeatmap {
   final List<String> rows;
   final List<String> columns;
-  final List items;
+  final List<double> items;
+  final List<List<int>> sensor_locations;
+  final Image heatmap_image;
 
-  DynamicHeatmap(
-      {required this.rows, required this.columns, required this.items});
+  DynamicHeatmap({
+    required this.rows,
+    required this.columns,
+    required this.items,
+    required this.sensor_locations,
+    required this.heatmap_image,
+  });
 }
 
 Future<DynamicHeatmap> getHeatmapData() async {
@@ -24,7 +31,7 @@ Future<DynamicHeatmap> getHeatmapData() async {
     };
     // your endpoint and request method
     var request = http.MultipartRequest(
-        'GET', Uri.parse('127.0.0.1:8000/api/v1/get_heatmap/'));
+        'GET', Uri.parse('http://127.0.0.1:8000/api/v1/get_heatmap/'));
 
     request.headers.addAll(headers);
 
@@ -34,16 +41,58 @@ Future<DynamicHeatmap> getHeatmapData() async {
       String newMessage = await response.stream.bytesToString();
       dynamic data = jsonDecode(newMessage);
 
+      List<List<int>> sensor_locs_temp = [];
+      List<int> locs_temp = [];
+      for (int i = 0; i < data['sensor_locations'].length; i++) {
+        locs_temp = [];
+        for (int j = 0; j < 2; j++) {
+          locs_temp.add(data['sensor_locations'][i][j]);
+        }
+        sensor_locs_temp.add(locs_temp);
+      }
+
+      final String encodedImage = data['heatmap_image'];
+      final Uint8List decodedImage = base64.decode(encodedImage);
+
       DynamicHeatmap result = DynamicHeatmap(
-          rows: data['rows'], columns: data['columns'], items: data['items']);
+          rows: List<String>.from(data['rows']),
+          columns: List<String>.from(data['columns']),
+          items: List<double>.from(data['items']),
+          sensor_locations: sensor_locs_temp,
+          heatmap_image: Image.memory(
+            decodedImage,
+            fit: BoxFit.cover,
+          ));
 
       return result;
     } else {
       print(response.reasonPhrase);
-      return DynamicHeatmap(rows: [], columns: [], items: []);
+      return DynamicHeatmap(
+        rows: ['5'],
+        columns: ['10'],
+        items: [10],
+        sensor_locations: [
+          [0, 0]
+        ],
+        heatmap_image: Image.asset(
+          'assets/icons/launcher_icon.png',
+          fit: BoxFit.cover,
+        ),
+      );
     }
   } catch (e) {
     print(e);
-    return DynamicHeatmap(rows: [], columns: [], items: []);
+    return DynamicHeatmap(
+      rows: ['5'],
+      columns: ['5', '10'],
+      items: [10, 15],
+      sensor_locations: [
+        [0, 0]
+      ],
+      heatmap_image: Image.asset(
+        'assets/icons/launcher_icon.png',
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
