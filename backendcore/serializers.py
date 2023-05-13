a0856e450aeb79de82ab5b0fbfa092f15a4527a2
Kursat Kaya <kursat@Kursats-MacBook-Pro.local>
2023-05-13 16:55:49 +0300

    Added serializer for report.

--- backendcore/serializers.py
+++ backendcore/serializers.py
@@ -8,15 +8,29 @@ class BooleanAsIntegerField(serializers.IntegerField):
             return None
         return int(value)
 
+
 class FarmParcelReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = FarmParcelReport
         fields = '__all__'
 
 
+class FarmReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FarmParcelReport
+        fields = ['moisture', 'phosphorus', 'potassium', 'nitrogen', 'temperature', 'ph']
+
+
 class FarmSerializer(serializers.ModelSerializer):
     is_active = BooleanAsIntegerField()
+    latest_farm_report = serializers.SerializerMethodField()
+
     class Meta:
         model = Farm
         fields = '__all__'
 
+    def get_latest_farm_report(self, obj):
+        latest_report = obj.farmparcelreport_set.order_by('-created_at').first()
+        if latest_report:
+            return FarmReportSerializer(latest_report).data
+        return None
