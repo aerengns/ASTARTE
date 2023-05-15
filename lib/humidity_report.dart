ab2ce7a279f764fcbb928cbfdc3e2ad7c6e34867
Nuray <nuray@Nurays-MacBook-Pro.local>
2023-05-15 19:21:00 +0300

    farm selection on reports page

--- lib/humidity_report.dart
+++ lib/humidity_report.dart
@@ -1,5 +1,7 @@
 import 'package:astarte/network_manager/services/sensor_data_service.dart';
+import 'package:astarte/theme/colors.dart';
 import 'package:astarte/utils/parameters.dart';
+import 'package:astarte/utils/reports_util.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:astarte/sidebar.dart';
@@ -19,11 +21,13 @@ class HumidityReport extends StatefulWidget {
 class _ReportsState extends State<HumidityReport> {
   List<double> data_y = [];
   String data_x = "";
+  List<Farm> farms = [];
+  String selectedFarm = "";
 
   @override
   void initState() {
     super.initState();
-    getHumidityData().whenComplete(() => _addHumidityContainer());
+    fillFarms().whenComplete(() => _addDropdown());
   }
 
   final List<Widget> _widgets = [
@@ -33,6 +37,7 @@ class _ReportsState extends State<HumidityReport> {
       height: 100,
       fit: BoxFit.contain,
     ),
+    const Text("Select a Farm"),
   ];
 
   @override
@@ -48,8 +53,40 @@ class _ReportsState extends State<HumidityReport> {
     );
   }
 
+  void _addDropdown() {
+    setState(() {
+      _widgets.add(
+        Center(
+          child: DropdownButton<String>(
+            value: selectedFarm,
+            icon: const Icon(
+              Icons.arrow_drop_down_circle,
+              color: CustomColors.astarteRed,
+            ),
+            onChanged: (value) {
+              setState(() {
+                selectedFarm = value!;
+              });
+              getHumidityData(selectedFarm)
+                  .whenComplete(() => _addHumidityContainer());
+            },
+            items: farms.map((farm) {
+              return DropdownMenuItem<String>(
+                value: farm.id,
+                child: Text(farm.name),
+              );
+            }).toList(),
+          ),
+        ),
+      );
+    });
+  }
+
   void _addHumidityContainer() {
     setState(() {
+      // Remove the old report container if it exists
+      _widgets.removeWhere((widget) =>
+          widget is SizedBox && widget.width == 450 && widget.height == 300);
       _widgets.add(
         SizedBox(
           width: 450,
@@ -87,16 +124,17 @@ class _ReportsState extends State<HumidityReport> {
     });
   }
 
-  Future<void> getHumidityData() async {
+  Future<void> getHumidityData(String selectedFarm) async {
     final response =
         await Provider.of<SensorDataService>(context, listen: false)
-            .getHumidityReport();
+            .getHumidityReport(selectedFarm);
 
     if (response.isSuccessful) {
       String new_message = await response.bodyString;
       dynamic data = jsonDecode(new_message);
 
       data_x = jsonEncode(data['days']);
+      data_y.clear();
       for (dynamic humidity_level in data['humidity_levels']) {
         data_y.add(humidity_level as double);
       }
@@ -104,6 +142,27 @@ class _ReportsState extends State<HumidityReport> {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Humidity report failed to load. Please try again.'),
+        ),
+      );
+    }
+  }
+
+  Future<void> fillFarms() async {
+    final response =
+        await Provider.of<SensorDataService>(context, listen: false)
+            .getFarmList();
+
+    if (response.isSuccessful) {
+      String new_message = await response.bodyString;
+      dynamic data = jsonDecode(new_message);
+      for (dynamic n in data) {
+        farms.add(Farm(name: n[0], id: n[1].toString()));
+      }
+      selectedFarm = farms[0].id;
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Temperature report failed to load. Please try again.'),
         ),
       );
     }
