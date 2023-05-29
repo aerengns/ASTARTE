0ad96a7a1578a183442c6cde0e7009f84593e8c9
Kursat Kaya <kursat@Kursats-MacBook-Pro.local>
2023-05-29 22:06:45 +0300

    Points

--- backendcore/serializers.py
+++ backendcore/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import FarmReport, Farm
+from .models import FarmReport, Farm, FarmCornerPoint
 
 
 class BooleanAsIntegerField(serializers.IntegerField):
@@ -15,9 +15,16 @@ class FarmReportSerializer(serializers.ModelSerializer):
         fields = ['moisture', 'phosphorus', 'potassium', 'nitrogen', 'temperature', 'ph']
 
 
+class FarmCornerPointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FarmCornerPoint
+        fields = ['latitude', 'longitude']
+
+
 class FarmSerializer(serializers.ModelSerializer):
     is_active = BooleanAsIntegerField()
     latest_farm_report = serializers.SerializerMethodField()
+    corner_points = FarmCornerPointSerializer(many=True)
 
     class Meta:
         model = Farm
@@ -28,3 +35,16 @@ class FarmSerializer(serializers.ModelSerializer):
         if latest_report:
             return FarmReportSerializer(latest_report).data
         return None
+
+    def get_corner_points(self, obj):
+        farm_corner_points = obj.farmcornerpoint_set.all()
+        if farm_corner_points:
+            return FarmCornerPointSerializer(farm_corner_points).data
+        return None
+
+    def create(self, validated_data):
+        corner_points_data = validated_data.pop('corner_points')
+        farm = Farm.objects.create(**validated_data)
+        for corner_point_data in corner_points_data:
+            FarmCornerPoint.objects.create(farm=farm, **corner_point_data)
+        return farm
