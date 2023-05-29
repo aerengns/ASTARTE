76345355fdb5b7b9990d4d3fb0f9c7c221cddc7c
Kursat Kaya <kursat@Kursats-MacBook-Pro.local>
2023-05-29 14:59:57 +0300

    Added necessary token system.

--- calendarapp/views.py
+++ calendarapp/views.py
@@ -19,3 +19,30 @@ class CalendarDataAPI(APIView):
                   Event.objects.filter(date__gte=datetime.now() - timedelta(weeks=12))]
 
         return Response(data={'events': events})
+
+
+class SendTokenAPI(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        if request.method == 'POST':
+            self.token = request.POST.get('token')
+            # self.send_notification()
+            # TODO - save the token to a database
+            return HttpResponse('Token received!')
+        else:
+            return HttpResponse('Invalid request method!')
+
+    def send_notification(self):
+        registration_token = self.token
+        title = 'Test Notification'
+        body = 'This is a test notification from Django!'
+        message = messaging.Message(
+            notification=messaging.Notification(
+                title=title,
+                body=body,
+            ),
+            token=registration_token,
+        )
+        response = messaging.send(message)
+        print('Successfully sent message:', response)
