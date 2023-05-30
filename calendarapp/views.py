e7d8efcd7b7a7c7ce2d5be787b99553c2ac6ff25
Aylin Topcu <topcu.aylinn@gmail.com>
2023-05-31 00:18:14 +0300

    remove duplicate code

--- calendarapp/views.py
+++ calendarapp/views.py
@@ -39,30 +39,3 @@ class CreateEventAPI(APIView):
         date = datetime.strptime(date, '%Y-%m-%d')
         CustomEvent.objects.create(description=description, date=date)
         return Response({'message': 'Event created'}, status=status.HTTP_201_CREATED)
-
-
-class SendTokenAPI(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        if request.method == 'POST':
-            self.token = request.POST.get('token')
-            # self.send_notification()
-            # TODO - save the token to a database
-            return HttpResponse('Token received!')
-        else:
-            return HttpResponse('Invalid request method!')
-
-    def send_notification(self):
-        registration_token = self.token
-        title = 'Test Notification'
-        body = 'This is a test notification from Django!'
-        message = messaging.Message(
-            notification=messaging.Notification(
-                title=title,
-                body=body,
-            ),
-            token=registration_token,
-        )
-        response = messaging.send(message)
-        print('Successfully sent message:', response)
