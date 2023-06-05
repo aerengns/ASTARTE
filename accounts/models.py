e4737f334972afb95ee03995c6d77629c6686b7b
Ahmet Eren Genis <e2448389@ceng.metu.edu.tr>
2023-06-05 18:01:15 +0300

    Fixed event creator notification and fixed device token notfication bug

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 
 class AbstractProfile(models.Model):
@@ -41,3 +42,4 @@ def save_user_profile(sender, instance, **kwargs):
 class DeviceToken(models.Model):
     token = models.CharField(max_length=200)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
